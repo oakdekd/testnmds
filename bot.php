4458132be09cfe60ef9de844ae10322d1bb46bec
oakdekd <76862224+oakdekd@users.noreply.github.com>
2021-01-02 14:30:20 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,56 @@
+<?php
+
+
+$API_URL = 'https://api.line.me/v2/bot/message';
+$ACCESS_TOKEN = 'xXyWz+UOFxo3bFFJ2svaNasqLzb8WuW+MO29HDmIann9u5Hu7fsD4xeJGY5/RPFJn+hnsqt4TY0LqgE/I0GsmD9mIwzlI4gc2adw2We0Fsz7VZ2oORHtYfQKycrRSCh5gMDu/wE5Gm1KZh/jGZu1dQdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '77dfc50e8646c219e5ff4b391e32d01e';
+
+
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+
+
+if ( sizeof($request_array['events']) > 0 ) {
+
+    foreach ($request_array['events'] as $event) {
+    
+    $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+        
+    }
+}
+
+echo "OK";
+
+
+
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+?>
